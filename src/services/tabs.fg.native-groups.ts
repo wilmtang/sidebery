d883cfea265de0c36c6b829e186d064d5b76739c
wilmtang <wilm.tang@outlook.com>
2026-05-26 04:16:49 -0700

    Bump version to 6.1.0

--- src/services/tabs.fg.native-groups.ts
+++ src/services/tabs.fg.native-groups.ts
@@ -159,7 +159,7 @@ export function isTabVisibleInNativeGroup(tab?: T.Tab): boolean {
   if (!hasNativeGroup(tab)) return true
   if (!tab) return true
   if (!isNativeGroupCollapsed(tab.groupId)) return true
-  return tab.active
+  return false
 }
 
 export function shouldShowNativeGroupBeforeTab(tab?: T.Tab, prevTab?: T.Tab): boolean {
